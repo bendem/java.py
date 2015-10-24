dd8159295803e4b484b0b0fa6a821658d43070fc
bendem <online@bendem.be>
2015-10-24 21:18:45 +0200

    Refactor everything into functions

--- java.py
+++ java.py
@@ -5,6 +5,92 @@ import os
 import subprocess
 import sys
 
+verbose   = False
+pretty    = False
+setup     = ''
+classpath = []
+code_args = []
+
+javac_args = ''
+java_args  = ''
+OUT        = '/tmp'
+CLASS      = 'Paul'
+SOURCE     = '%s.java' % CLASS
+COMPILED   = '%s.class' % CLASS
+TEMPLATE   = """
+    import java.io.*;
+    import java.math.*;
+    import java.nio.*;
+    import java.nio.charset.*;
+    import java.nio.file.*;
+    import java.time.*;
+    import java.time.chrono.*;
+    import java.time.format.*;
+    import java.time.temporal.*;
+    import java.time.zone.*;
+    import java.util.*;
+    import java.util.concurrent.*;
+    import java.util.concurrent.atomic.*;
+    import java.util.concurrent.locks.*;
+    import java.util.function.*;
+    import java.util.stream.*;
+    %s
+
+    public class %s {
+        public static void main(String[] args) throws Exception {
+            %s;
+            %s
+        }
+    }
+    """
+OUTPUT_CODE_TEMPLATE = """
+    Object ಠ_ಠ = %s;
+    Stream<?> ϟ = null;
+    if(ಠ_ಠ instanceof Object[]) {
+        ϟ = Arrays.stream((Object[]) ಠ_ಠ);
+
+    } else if(ಠ_ಠ instanceof int[]) {
+        ϟ = Arrays.stream((int[]) ಠ_ಠ).mapToObj(Integer::valueOf);
+    } else if(ಠ_ಠ instanceof long[]) {
+        ϟ = Arrays.stream((long[]) ಠ_ಠ).mapToObj(Long::valueOf);
+    } else if(ಠ_ಠ instanceof double[]) {
+        ϟ = Arrays.stream((double[]) ಠ_ಠ).mapToObj(Double::valueOf);
+
+    } else if(ಠ_ಠ instanceof Collection<?>) {
+        ϟ = ((Collection<?>) ಠ_ಠ).stream();
+    } else if(ಠ_ಠ instanceof Stream<?>) {
+        ϟ = (Stream<?>) ಠ_ಠ;
+
+    } else if(ಠ_ಠ instanceof IntStream) {
+        ϟ = ((IntStream) ಠ_ಠ).boxed();
+    } else if(ಠ_ಠ instanceof LongStream) {
+        ϟ = ((LongStream) ಠ_ಠ).boxed();
+    } else if(ಠ_ಠ instanceof DoubleStream) {
+        ϟ = ((DoubleStream) ಠ_ಠ).boxed();
+    }
+
+    if(ϟ == null) {
+        System.out.println(ಠ_ಠ);
+    } else {
+        System.out.println(
+            ϟ
+                .map(ツ -> {
+                    if(ツ == null) {
+                        return "null";
+                    }
+                    if(ツ instanceof Character) {
+                        return "'" + ツ + "'";
+                    }
+                    if(ツ instanceof Number) {
+                        return ツ.toString();
+                    }
+                    return "\\"" + ツ + '"';
+                })
+                .collect(Collectors.joining(",%s", "[", "]"))
+        );
+    }
+"""
+
 def help():
     print(' Usage is ./java.py [options] <code>')
     print()
@@ -16,166 +102,103 @@ def help():
     print('    -cp Adds a jar to the classpath')
     sys.exit()
 
-if len(sys.argv) == 1:
-    help()
+def parse_args(args):
+    global verbose, pretty, setup, classpath, code_args
 
-verbose   = False
-pretty    = False
-setup     = ''
-classpath = []
-code_args = []
+    if len(args) == 1:
+        help()
 
-arg_it = iter(sys.argv)
-next(arg_it)
-try:
-    while True:
-        x = next(arg_it)
-        if x == '-s':
-            setup = next(arg_it)
-            setup = setup.strip(';') + ';'
-        elif x == '-p':
-            pretty = True
-        elif x == '-v':
-            verbose = True
-        elif x == '-cp':
-            classpath.append(next(arg_it))
-        elif x == '-h' or x == '--help':
-            help()
-        else:
-            code_args.append(x)
-except StopIteration:
-    pass
+    arg_it = iter(args)
+    next(arg_it)
+    try:
+        while True:
+            x = next(arg_it)
+            if x == '-s':
+                setup = next(arg_it)
+                setup = setup.strip(';') + ';'
+            elif x == '-p':
+                pretty = True
+            elif x == '-v':
+                verbose = True
+            elif x == '-cp':
+                classpath.append(next(arg_it))
+            elif x == '-h' or x == '--help':
+                help()
+            else:
+                code_args.append(x)
+    except StopIteration:
+        pass
 
-if not code_args:
-    help()
+    if not code_args:
+        help()
 
-OUT        = '/tmp'
-CLASS      = 'Paul'
-SOURCE     = '%s.java' % CLASS
-COMPILED   = '%s.class' % CLASS
-javac_args = ''
-java_args  = ''
+def generate_code():
+    # something something backslash something
+    code = ' '.join(code_args).strip(';').replace('\\', '\\\\').split(';')
+    last_instr = code[-1].strip()
+    output = ''
 
-# something something backslash something
-code = ' '.join(code_args).strip(';').replace('\\', '\\\\').split(';')
-last_instr = code[-1].strip()
-output = ''
+    # Auto display formatting thingy stuff
+    if not 'System.out.print' in last_instr and not last_instr.startswith('}'):
+        output = OUTPUT_CODE_TEMPLATE % (code[-1], ('\\n' if pretty else ' '))
+        del code[-1]
 
-# Auto display formatting thingy stuff
-if not 'System.out.print' in last_instr and not last_instr.startswith('}'):
-    output = """
-        Object ಠ_ಠ = %s;
-        Stream<?> ϟ = null;
-        if(ಠ_ಠ instanceof Object[]) {
-            ϟ = Arrays.stream((Object[]) ಠ_ಠ);
+    return TEMPLATE % (setup, CLASS, ';'.join(code), output)
 
-        } else if(ಠ_ಠ instanceof int[]) {
-            ϟ = Arrays.stream((int[]) ಠ_ಠ).mapToObj(Integer::valueOf);
-        } else if(ಠ_ಠ instanceof long[]) {
-            ϟ = Arrays.stream((long[]) ಠ_ಠ).mapToObj(Long::valueOf);
-        } else if(ಠ_ಠ instanceof double[]) {
-            ϟ = Arrays.stream((double[]) ಠ_ಠ).mapToObj(Double::valueOf);
+def write_to_file(code, file):
+    # with open('%s/%s' % (OUT, SOURCE), 'w') as f:
+    with open(file, 'w') as f:
+        f.write(code)
 
-        } else if(ಠ_ಠ instanceof Collection<?>) {
-            ϟ = ((Collection<?>) ಠ_ಠ).stream();
-        } else if(ಠ_ಠ instanceof Stream<?>) {
-            ϟ = (Stream<?>) ಠ_ಠ;
+def compile():
+    args = javac_args + ' -nowarn'
+    if classpath:
+        args += ' -cp %s' % ':'.join(classpath)
 
-        } else if(ಠ_ಠ instanceof IntStream) {
-            ϟ = ((IntStream) ಠ_ಠ).boxed();
-        } else if(ಠ_ಠ instanceof LongStream) {
-            ϟ = ((LongStream) ಠ_ಠ).boxed();
-        } else if(ಠ_ಠ instanceof DoubleStream) {
-            ϟ = ((DoubleStream) ಠ_ಠ).boxed();
-        }
+    javac = 'javac %s -d %s %s/%s' % (args, OUT, OUT, SOURCE)
+    javac = javac.replace('  ', ' ')
 
-        if(ϟ == null) {
-            System.out.println(ಠ_ಠ);
-        } else {
-            System.out.println(
-                ϟ
-                    .map(ツ -> {
-                        if(ツ == null) {
-                            return "null";
-                        }
-                        if(ツ instanceof Character) {
-                            return "'" + ツ + "'";
-                        }
-                        if(ツ instanceof Number) {
-                            return ツ.toString();
-                        }
-                        return "\\"" + ツ + '"';
-                    })
-                    .collect(Collectors.joining(",%s", "[", "]"))
-            );
-        }
-    """ % (code[-1], ('\\n' if pretty else ' '))
-    del code[-1]
+    if verbose:
+        print('%% %s' % javac)
 
-with open('%s/%s' % (OUT, SOURCE), 'w') as f:
-    f.write(
-        """
-        import java.io.*;
-        import java.math.*;
-        import java.nio.*;
-        import java.nio.charset.*;
-        import java.nio.file.*;
-        import java.time.*;
-        import java.time.chrono.*;
-        import java.time.format.*;
-        import java.time.temporal.*;
-        import java.time.zone.*;
-        import java.util.*;
-        import java.util.concurrent.*;
-        import java.util.concurrent.atomic.*;
-        import java.util.concurrent.locks.*;
-        import java.util.function.*;
-        import java.util.stream.*;
-        %s
+    p = subprocess.Popen(javac, shell = True, stdout = subprocess.PIPE,
+        stderr = subprocess.STDOUT)
 
-        public class %s {
-            public static void main(String[] args) throws Exception {
-                %s;
-                %s
-            }
-        }
-        """ % (setup, CLASS, ';'.join(code), output)
-    )
+    for line in p.stdout.readlines():
+        print(' | %s' % line.decode().strip())
 
-javac_args += ' -nowarn'
-if classpath:
-    javac_args += ' -cp %s' % ':'.join(classpath)
+    if p.wait() != 0:
+        print('Compilation failed')
+        cleanup(False)
+        sys.exit(-1)
+        return False
 
-javac = 'javac %s -d %s %s/%s' % (javac_args, OUT, OUT, SOURCE)
+    return True
 
-if verbose:
-    print('%% %s' % javac.replace('  ', ' '))
+def run():
+    args = java_args + ' -cp %s' % ':'.join(classpath + [OUT])
+    java = 'java %s %s' % (args, CLASS)
+    java = java.replace('  ', ' ')
 
-p = subprocess.Popen(javac, shell = True, stdout = subprocess.PIPE,
-    stderr = subprocess.STDOUT)
+    if verbose:
+        print('%% %s' % java)
 
-for line in p.stdout.readlines():
-    print(' | %s' % line.decode().strip())
+    execution = subprocess.Popen(java, shell = True, stdout = subprocess.PIPE,
+        stderr = subprocess.STDOUT)
 
-if p.wait() != 0:
-    print('Compilation failed')
+    for line in execution.stdout.readlines():
+        print(' >> %s' % line.decode().strip())
+
+    execution.wait()
+
+def cleanup(compiled = True):
     os.remove('%s/%s' % (OUT, SOURCE))
-    sys.exit(-1)
+    if compiled:
+        os.remove('%s/%s' % (OUT, COMPILED))
 
-
-java_args += ' -cp %s' % ':'.join(classpath + [OUT])
-java = 'java %s %s' % (java_args, CLASS)
-
-if verbose:
-    print('%% %s' % java.replace('  ', ' '))
-
-execution = subprocess.Popen(java, shell = True, stdout = subprocess.PIPE,
-    stderr = subprocess.STDOUT)
-
-for line in execution.stdout.readlines():
-    print(' >> %s' % line.decode().strip())
-
-execution.wait()
-
-os.remove('%s/%s' % (OUT, SOURCE))
-os.remove('%s/%s' % (OUT, COMPILED))
+if __name__ == '__main__':
+    parse_args(sys.argv)
+    write_to_file(generate_code(), '%s/%s' % (OUT, SOURCE))
+    if compile():
+        run()
+        cleanup()
